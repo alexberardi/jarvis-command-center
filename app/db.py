b8528ada791ba76664f3275c5cecc921726196d7
alexberardi <alexmberardi@gmail.com>
2025-03-24 23:09:30 -0400

    Fix database path

--- app/db.py
+++ app/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.models import Base
 
-SQLITE_URL = "sqlite:///./voice_api.db"
+SQLITE_URL = "sqlite:///./data/voice_api.db"
 engine = create_engine(SQLITE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
